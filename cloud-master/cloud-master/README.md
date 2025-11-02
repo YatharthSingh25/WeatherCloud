1b623000f534e273f6d0aa4c32ab71798bb18134
studydock <yatharthsingh74644@gmail.com>
2025-11-03 02:55:27 +0530

    Delete cloud-master directory

--- cloud-master/cloud-master/README.md
+++
@@ -1,3 +0,0 @@
-# cloud.
-
-<img src="./public/ss/cloud-ss-v2-c.png" alt="" />
--- cloud-master/cloud-master/js/day.js
+++
@@ -1,4 +0,0 @@
-// Importing external library
-import dayjs from "https://unpkg.com/dayjs@1.11.13/esm/index.js";
-
-export const today = dayjs();
